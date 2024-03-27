99bb2fde72878e7390092ba33a3f8ec3cae18301
Pedro Rosa <contatophique@gmail.com>
2024-03-26 23:08:19 -0300

    Transaction DTO's created

--- server/src/main/java/phique/payflux/dto/TransactionRequestDTO.java
+++ server/src/main/java/phique/payflux/dto/TransactionRequestDTO.java
@@ -1,6 +1,12 @@
 package phique.payflux.dto;
 
+import phique.payflux.model.Transaction;
+
+import java.math.BigDecimal;
 import java.util.Date;
 
 public record TransactionRequestDTO(String name_trade, String type_trade, double amount, String tag, Date date) {
+    public TransactionRequestDTO(Transaction transaction) {
+        this(transaction.getName_trade(), transaction.getType_trade(), transaction.getAmount(), transaction.getTag(), transaction.getDate());
+    }
 }
--- server/src/main/java/phique/payflux/dto/TransactionResponseDTO.java
+++ server/src/main/java/phique/payflux/dto/TransactionResponseDTO.java
@@ -2,6 +2,7 @@ package phique.payflux.dto;
 
 import phique.payflux.model.Transaction;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.UUID;
 
